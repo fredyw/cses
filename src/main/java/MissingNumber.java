38637d98755696d5820355d8286760fa9baa99b1
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-06 23:29:02 -0600

    Skeleton for Missing Number problem

---
+++ src/main/java/MissingNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * https://cses.fi/problemset/task/1083
+ */
+public class MissingNumber {
+    private static int solve(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        var n = input.nextInt();
+        for (var i = 0; i < n; i++) {
+            // TODO
+        }
+    }
+}
