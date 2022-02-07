fdefec3297d5705e9d6bb07273e88974ad0cc0d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-06 23:22:28 -0600

    Solve Weird Algorithm

---
+++ src/main/java/WeirdAlgorithm.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+    private static void solve(long n) {
+        var m = n;
+        while (m != 1) {
+            System.out.print(m + " ");
+            if (m % 2 == 0) {
+                m /= 2;
+            } else {
+                m = (m * 3) + 1;
+            }
+        }
+        System.out.print(1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        solve(input.nextLong());
+    }
+}
