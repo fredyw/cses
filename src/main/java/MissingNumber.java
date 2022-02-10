6e45f727a0494ab5d4f4163c37b1d43e0e9b1b15
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-09 22:29:35 -0600

    Solve Missing Number problem

--- src/main/java/MissingNumber.java
+++ src/main/java/MissingNumber.java
@@ -1,19 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * https://cses.fi/problemset/task/1083
  */
 public class MissingNumber {
-    private static int solve(int n) {
-        // TODO
-        return 0;
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         var n = input.nextInt();
-        for (var i = 0; i < n; i++) {
-            // TODO
+        var allSum = 0;
+        for (var i = 1; i <= n; i++) {
+            allSum += i;
         }
+        input.nextLine();
+        var sum = Arrays.stream(input.nextLine().split(" "))
+            .mapToInt(e -> Integer.parseInt(e))
+            .sum();
+        System.out.print(allSum - sum);
     }
 }
