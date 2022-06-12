94891fd57650f855f51239d308e7cbf9663fd8f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-12 10:50:38 -0500

    Solve Permutations problem

--- src/bin/permutations.rs
+++ src/bin/permutations.rs
@@ -1,2 +1,26 @@
+use std::io;
+
 // https://cses.fi/problemset/task/1070
-fn main() {}
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let n = line.trim().parse::<i32>().unwrap();
+    if n == 1 {
+        println!("1");
+    } else if n == 2 || n == 3 {
+        println!("NO SOLUTION");
+    } else {
+        let mut j = 2;
+        for _i in 0..n {
+            if j == 2 {
+                print!("{}", j);
+            } else {
+                print!(" {}", j);
+            }
+            j += 2;
+            if j > n {
+                j = 1;
+            }
+        }
+    }
+}
