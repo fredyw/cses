87d3922b80b3c8cb433ae9f5eb31a4fbc911a6d2
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-13 13:04:40 -0500

    Solve Number Spiral problem

--- src/bin/number_spiral.rs
+++ src/bin/number_spiral.rs
@@ -11,15 +11,27 @@ fn main() {
         let coord = line
             .trim()
             .split(" ")
-            .map(|e| e.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+            .map(|e| e.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
         let y = &coord[0];
         let x = &coord[1];
         let largest = cmp::max(y, x);
-        let min = i32::pow(*largest - 1, 2) + 1;
-        let max = i32::pow(*largest, 2);
+        let min = i64::pow(*largest - 1, 2);
+        let max = i64::pow(*largest, 2) + 1;
         let top = largest % 2 == 0;
-        let value = if top { todo!() } else { todo!() };
-        // println!("{}", value);
+        let value = if x == largest {
+            if top {
+                min + y
+            } else {
+                max - y
+            }
+        } else {
+            if top {
+                max - x
+            } else {
+                min + x
+            }
+        };
+        println!("{}", value);
     }
 }
